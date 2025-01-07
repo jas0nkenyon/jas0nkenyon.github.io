c38da267c9c67ad67b6391e6a26181f9ff40abbc
jason <jas0nkenyon@protonmail.com>
2025-01-06 19:55:46 -0500

    build(scope?): subject stuff

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1736210665',resources: [
+    cacheName: 'chirpy-1736211290',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
