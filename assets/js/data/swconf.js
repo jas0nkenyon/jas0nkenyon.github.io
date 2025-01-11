ecad59ab8973984ae29ca84b241ab25bb1f1c170
jason <jas0nkenyon@protonmail.com>
2025-01-11 14:18:49 -0500

    feat(global?): added relevant and internal

--- assets/js/data/swconf.js
+++ assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1736211733',resources: [
+    cacheName: 'chirpy-1736623121',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
